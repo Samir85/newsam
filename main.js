cbbaba1c6a13007fa6d831e56d2c0da32e5eaeb7
Samir85 <rakroukisamir0@gmail.com>
2023-06-15 21:28:30 +0100

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.imag');
+let container = document.querySelector('.container')
+
+function Phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background = color ;
+}
